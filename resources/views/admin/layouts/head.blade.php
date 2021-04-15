773b2901397ecb33d754a99c813bea40b8f15097
Jackarly <jcbermudez3033@gmail.com>
2021-04-15 13:42:28 +0800

    Fixed bug from adding custom css.

--- resources/views/admin/layouts/head.blade.php
+++ resources/views/admin/layouts/head.blade.php
@@ -8,6 +8,9 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Agri-Higala</title>
   
+    <!-- Custom styles from scss-->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    
     <!-- Custom fonts for this template-->
     <link href="{{asset('backend/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
@@ -16,7 +19,5 @@
     <link href="{{asset('backend/css/sb-admin-2.min.css')}}" rel="stylesheet">
     @stack('styles') 
 
-    <!-- Custom styles from scss-->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
   
 </head>
